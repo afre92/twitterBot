4f91b9c8a9d5f7c5414a68af38c34c17ce97427a
Andres <afre92@gmail.com>
2022-06-07 14:06:28 +0200

    feat: creadentials feature and twitter tokens added

--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -1,14 +1,25 @@
 require 'tweetkit'
+require 'httparty'
 
 module TwitterClient
- 
- client = Tweetkit::Client.new(bearer_token: 'YOUR_BEARER_TOKEN_HERE',
- 															consumer_key: 'YOUR_CONSUMER_KEY_HERE',
- 															consumer_secret: 'YOUR_CONSUMER_SECRET_HERE')
+	client = Tweetkit::Client.new(bearer_token: Rails.application.credentials.twitter.bearer_token,
+														api_key: Rails.application.credentials.twitter.api_key,
+														api_secret_key: Rails.application.credentials.twitter.api_secret_key)
 
- action to check price
-
+	def create_post
+		client.post('')
+	end
 end
 
 
-# maybe have to make it a class
+# # maybe have to make it a class
+
+# module Tweetkit
+#   class << self
+#     def client
+#       return @client if defined?(@client) && @client.same_options?(options)
+
+#       @client = Tweetkit::Client.new(options)
+#     end
+#   end
+# end
