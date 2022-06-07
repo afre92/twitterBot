7a985310d471d2e7a9efcf550e3b703337918084
Andres <afre92@gmail.com>
2022-06-07 14:09:33 +0200

    fix: clean up and indentation fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,37 +1,27 @@
 class HomeController < ApplicationController
-	require 'httparty'
-	require 'tweetkit'
+  require 'httparty'
+  require 'tweetkit'
 
-	def run
-		# price = check_price
-		# render "this is the price #{price}"
+  def run
+    response = create_post
+    puts response
+  end
 
-		# if price_has_changed
-		# 	# create post
-		# 	# post it to twitter
-		# else
-		# end
+  private
 
-		response = create_post
-		puts response
-	end
+    def create_post
+      client = Tweetkit::Client.new(bearer_token:        Rails.application.credentials.twitter[:bearer_token],
+      consumer_key:        Rails.application.credentials.twitter[:consumer_key],
+      consumer_secret: 		 Rails.application.credentials.twitter[:consumer_secret],
+      access_token: 			 Rails.application.credentials.twitter[:access_token],
+      access_token_secret: Rails.application.credentials.twitter[:access_token_secret])
 
-	private
+      client.post_tweet({text: "Hello world!!!"})
+    end
 
-		def create_post
- 		  client = Tweetkit::Client.new(bearer_token:        Rails.application.credentials.twitter[:bearer_token],
- 																		consumer_key:        Rails.application.credentials.twitter[:consumer_key],
- 																		consumer_secret: 		 Rails.application.credentials.twitter[:consumer_secret],
- 																		access_token: 			 Rails.application.credentials.twitter[:access_token],
- 																		access_token_secret: Rails.application.credentials.twitter[:access_token_secret])
-
- 		  # client = Tweetkit::Client.new(bearer_token: , consumer_key: 'YOUR_CONSUMER_KEY_HERE', consumer_secret: 'YOUR_CONSUMER_SECRET_HERE')
- 		  client.post_tweet({text: "Hello world!!!"})
-		end
-
-		def check_price
-			response = HTTParty.get("https://data.messari.io/api/v1/assets/harmony/metrics?x-messari-api-key=#{Rails.application.credentials.messari[:api_key]}")
-			response["data"]["market_data"]["price_usd"]
-		end
+    def check_price
+      response = HTTParty.get("https://data.messari.io/api/v1/assets/harmony/metrics?x-messari-api-key=#{Rails.application.credentials.messari[:api_key]}")
+      response["data"]["market_data"]["price_usd"]
+    end
 
 end
